7503ef5117b56ec42114067f18f9d779306052f7
SKT-T1-Thecai <56053562+SKT-T1-Thecai@users.noreply.github.com>
2020-10-07 23:55:47 +0800

    Update program.java

--- program.java
+++ program.java
@@ -29,6 +29,10 @@ public class program{
             return "Else";
         else if(isAlpha(s.charAt(0)))
             return "Ident("+s+")";
+        else if(s.equals(":"))
+            return "Colon";
+        else if(s.equals(":="))
+            return "Assign";
         else {
 
             for(int i=0;i<s.length();i++)
@@ -39,83 +43,130 @@ public class program{
 
 
         }
-    return "";
+        return "";
     }
+
     public static void analyse(char[] str) {
         String s = new String(str);
-
         String[] arr = s.split("\\s+");
         int sign = 0;
+        int pos=0;
         for (int i = 0; i < arr.length; i++) {
 
-            char[] ch = arr[i].toCharArray();
-            for (int j = 0; j < ch.length; j++) {
-                if (j == 0) {
-                    if (isAlpha(ch[j]))
-                        sign = 1;
-                    else if (isInt(ch[j]))
-                        sign = 2;
-                    else if (ch[j] == '+' && ch.length == 1) {
-                        System.out.println("Plus");
-                        break;
-                    } else if (ch[j] == '*' && ch.length == 1) {
-                        System.out.println("Star");
-                        break;
-                    } else if (ch[j] == ',' && ch.length == 1) {
-                        System.out.println("Comma");
-                        break;
-                    } else if (ch[j] == '(' && ch.length == 1) {
-                        System.out.println("LParenthesis");
-                        break;
-                    } else if (ch[j] == ')' && ch.length == 1) {
-                        System.out.println("RParenthesis");
-                        break;
-                    } else if (ch[j] == ':' && ch.length == 2 && ch[j + 1] == '=') {
-                        System.out.println("Assign");
-                        break;
-                    } else if (ch[j] == ':' && ch.length == 1) {
-                        System.out.println("Colon");
-                        break;
-                    } else {
-                        System.out.println("Unknown1111111");
-                        return;
-                    }
-                } else {
-                    if (sign == 1) {
-                        if (!isAlpha(ch[j])&&!isInt(ch[j]))
-                        {
-                            System.out.println(ch);
-                            return;
-                        }
-                    }
-                    else
+            char[] ch=arr[i].toCharArray();// 001456asf
+            char[] stack=new char[ch.length];
+            for(int j=0;j<ch.length;j++)
+            {
+            //   System.out.println(j+" "+pos);
+                if(pos==0)
+                {
+                    if(isAlpha(ch[j]))
                     {
-                        if(isAlpha(ch[j]))
-                        {
-                            System.out.println(work(new String(ch).substring(0,j)));
-                            ch=new String(ch).substring(j,ch.length).toCharArray();
-                            j=-1;
-                            continue;
-                            //analyse(new String(ch).substring(j+1,ch.length+1).toCharArray());
-                        }
-                        else if(!isInt(ch[j]))
-                        {
-                            System.out.println("Unknown3333333333");
-                            return;
-                        }
+                        stack[pos++]=ch[j];
+                        sign=1;
+                    }
+                    else if(isInt(ch[j]))
+                    {
+                        stack[pos++]=ch[j];
+                        sign=2;
+                    }
+                    else if(ch[j]==':')
+                    {
+                        stack[pos++]=ch[j];
+                        sign=3;
+                    }
+                    else if(ch[j]=='+')
+                    {
+                        System.out.println("Plus");
+                        pos=0;
+                        continue;
+
+                    }
+                    else if(ch[j]=='*')
+                    {
+                        System.out.println("Star");
+                        pos=0;
+                        continue;
+
+                    }
+                    else if(ch[j]==',')
+                    {
+                        System.out.println("Comma");
+                        pos=0;
+                        continue;
+
+                    }
+                    else if(ch[j]=='(')
+                    {
+                        System.out.println("LParenthesis");
+                        pos=0;
+                        continue;
+
+                    }
+                    else if(ch[j]==')')
+                    {
+                        System.out.println("RParenthesis");
+                        pos=0;
+                        continue;
+
                     }
                 }
-                if(j==ch.length-1)
-                {
-                    System.out.println(work(new String(ch)));
+                else
+                    {
+                        if(sign==1)
+                    {
+                        if(!isInt(ch[j])&&!isAlpha(ch[j]))
+                        {
+                            System.out.println(work(new String(stack).substring(0,pos)));
+                            pos=0;
+                            j-=1;
+                            continue;
+                        }
+                        else stack[pos++]=ch[j];
                     }
+                    else if(sign==2)
+                    {
+                        if(!isInt(ch[j]))
+                        {
+
+                            System.out.println(work(new String(stack).substring(0,pos)));
+                            //System.out.println(j);
+                            pos=0;
+                           j-=1;
+                            continue;
+                        }
+                        else stack[pos++]=ch[j];
+                    }
+                    else if(sign==3)
+                    {
+                        if(ch[j]!='=')
+                        {
+                            System.out.println("Colon");
+                            pos=0;
+                            j-=1;
+                            //continue;
+                        }else {
+                            System.out.println("Assign");
+                            pos=0;
+                           // continue;
+                        }
+
+                    }
+
+                }
+                if(j==ch.length-1&&pos!=0)
+                {
+                    System.out.println(work(new String(stack).substring(0,pos)));
+                    pos=0;
+                    break;
                 }
             }
         }
+    }
 
     public static void main(String[] args) throws IOException {
-       
-       String Path=args[0];
+   
+        String Path=args[0];
         File  file=new File(Path);
         FileInputStream f=new FileInputStream(file);
         byte[] buf = new byte[4096];
